6b16fbb041ddad418bdafe63a0e04cdb68a80f43
daikidev111 <dkub0001@student.monash.edu>
2020-12-17 18:11:14 +0900

    test commit1

---
+++ bubble_sort.php
@@ -0,0 +1,21 @@
+<?php
+//Complexity O(n^2)
+
+function bubble_sort ($a_list) {
+	$n = count($a_list) - 1;
+	for ($i = 0; $i < $n; $i++) {
+		for ($j = 0; $j < $n - $i; $j++) {
+			$k = $j + 1;
+			if ($a_list[$k] < $a_list[$j]) {
+				list($a_list[$k], $a_list[$j]) = array($a_list[$j], $a_list[$k]);
+			}
+		}
+	}
+	return $a_list;
+}
+
+
+$arr = array(5, 6, 7, 1, 5, 2, 7);
+$sorted_arr = bubble_sort($arr);
+print_r($sorted_arr);
+
---
+++ movie_list.php
@@ -0,0 +1,17 @@
+<?php
+
+class MovieList {
+	private $stack;
+	private $limit;
+
+	public function __construct($limit) {
+		$this->$stack = array();
+		$this->$limit = $limit;
+	}
+
+}
+
+
+
+
+
